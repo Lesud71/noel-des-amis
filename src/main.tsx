5c1cffb43cdb6bea3bb889079233f14381f1ada6
Lesud71 <comamisgroup@gmail.com>
2026-08-26 15:57:03 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { NeonAuthUIProvider } from '@neondatabase/neon-js/auth/react'
 import '@neondatabase/neon-js/ui/css'
-import { neon } from './neon'
+import { authClient } from './neon'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
